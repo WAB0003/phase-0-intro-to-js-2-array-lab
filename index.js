7ef528597b5bcff1f59d4d59b79c0b7177bdb2c8
Bill Brown <bill.a.brown90@gmail.com>
2023-02-24 16:42:03 -0800

    Arrays again

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name)
+
+function destructivelyPrependCat(name)
+
+function destructivelyRemoveLastCat(name)
+
+function destructivelyRemoveFirstCat(name)
+
+function appendCat(name)
+
+function prependCat(name)
+
+function removeLastCat()
+
+function removeFirstCat()
